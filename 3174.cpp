343ad81b154b4c96fbf517af1b0e27b73612716f
MASJT <jaiveerjaykruti@gmail.com>
2024-11-01 23:31:03 +0530

    Clear Digits

--- 3174.cpp
+++ 3174.cpp
@@ -3,19 +3,19 @@ public:
     string clearDigits(string s) {
         stack<char> entities;
 
-        for (char c : s) {
-            if (isdigit(c)) {
-                if (!isdigit(entities.top()) && !entities.empty()) {
+        for(char c : s){
+            if(isdigit(c)){
+                if(!isdigit(entities.top()) && !entities.empty()) {
                     entities.pop();
                 }
             }
-            else {
+            else{
                 entities.push(c);
             }
         }
         
         string result;
-        while (!entities.empty()) {
+        while(!entities.empty()){
             result += entities.top();
             entities.pop();
         }
